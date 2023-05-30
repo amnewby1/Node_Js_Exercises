905a991d4b0e2a91837090220ef99e0d3cb90620
A_Simms <114841649+amnewby1@users.noreply.github.com>
2023-05-29 21:11:05 -0400

    Add files via upload

--- 33.4_Exercise/step3.js
+++ 33.4_Exercise/step3.js
@@ -1,20 +1,20 @@
 const fs = require("fs");
 const axios = require("axios");
 
-function cat(path) {
+function cat(path, outputFile) {
   fs.readFile(path, "utf8", (err, data) => {
     if (err) {
       console.log("Error", err);
       process.kill(1);
     }
-    console.log("Data...", data);
+    writeOutput(data, outputFile);
   });
 }
 
-async function webCat(url) {
+async function webCat(url, outputFile) {
   try {
     let response = await axios.get(url);
-    console.log(response.data);
+    writeOutput(response.data, outputFile);
   } catch (e) {
     console.log("Something went wrong", e);
     process.exit(1);
@@ -23,10 +23,30 @@ async function webCat(url) {
 
 //url I used for webCat: "https://www.poetryfoundation.org/poems/44263/fire-and-ice"
 
+function writeOutput(content, outputFile) {
+  if (outputFile) {
+    fs.writeFile(outputFile, content, (err) => {
+      if (err) {
+        console.log("Error writing to the file:", err);
+        process.exit(1);
+      }
+      console.log("Output written to file:", outputFile);
+    });
+  } else {
+    console.log("Output:", content);
+  }
+}
+
 let thirdArg = process.argv[2];
+let outputFile = null;
+
+if (thirdArg === "--out" && process.argv.length >= 5) {
+  outputFile = process.argv[3];
+  thirdArg = process.argv[4];
+}
 
 if (thirdArg.slice(0, 4) != "http") {
-  cat(thirdArg);
+  cat(thirdArg, outputFile);
 } else {
-  webCat(thirdArg);
+  webCat(thirdArg, outputFile);
 }
